b9c07e6a96e98363f262f00ebf63dd71a556ff50
Jacob Quinn <quinn.jacobd@gmail.com>
2021-02-25 10:17:35 -0700

    Fix Julia 1.0

--- src/JSONTypeProvider.jl
+++ src/JSONTypeProvider.jl
@@ -2,6 +2,12 @@ module JSONTypeProvider
 
 import JSON3
 
+@static if Base.VERSION < v"1.2"
+    function hasfield(::Type{T}, name::Symbol) where T
+        return name in fieldnames(T)
+    end
+end
+
 include("build_type.jl")
 include("write_type.jl")
 
